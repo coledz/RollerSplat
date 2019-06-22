7b45ea1ec365c587ff1835b12c55b278a1d9f3e8
coledz <coledzz21@gmail.com>
2019-06-22 12:24:15 -0500

    Add Roller Splat Game

---
+++ Roller Splat/Assets/Scripts/GroundPiece.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundPiece : MonoBehaviour
+{
+
+    public bool isColored = false;
+
+    public void ChangeColor(Color color)
+    {
+        GetComponent<MeshRenderer>().material.color = color;
+        isColored = true;
+
+        GameManager.singleton.CheckComplete();
+    }
+}
---
+++ Roller Splat/Assets/Scripts/LoseTrigger.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoseTrigger : MonoBehaviour
+{
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(2);
+        }
+    }
+
+}
